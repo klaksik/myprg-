463147cb1c7113c54a16808f9782c755d353a746
klaksik <89664351+klaksik@users.noreply.github.com>
2024-01-04 16:49:38 +0200

    Create CYOB.sh

---
+++ CYOB.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Обновление списка пакетов
+sudo apt-get update
+
+# Обновление установленных пакетов
+sudo apt-get upgrade -y
+
+# Установка OpenJDK 17 JDK и JRE
+sudo apt-get install -y openjdk-17-jdk openjdk-17-jre
+
+# Прямая ссылка на JAR файл
+jar_url="https://store11.gofile.io/download/2ab21f8e-42b6-46f0-96d5-068caae05a44/CYOB.jar"
+
+# Загрузка JAR файла
+wget "$jar_url" -O CYOB.jar
+
+# Запуск программы
+java -jar CYOB.jar
