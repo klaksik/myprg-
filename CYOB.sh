459eb0e161f80161ab3b5d6c3a2c1475b65ac847
klaksik <89664351+klaksik@users.noreply.github.com>
2024-01-06 22:18:48 +0200

    Update CYOB.sh

--- CYOB.sh
+++ CYOB.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# Путь к программе и JAR файлу
+program_name="CYOB"
+program_exec="java -jar $HOME/CYOB.jar"
+desktop_file="$HOME/.config/autostart/$program_name.desktop"
+
 # Обновление списка пакетов
 sudo apt-get update
 
@@ -13,8 +18,18 @@ sudo apt-get install -y openjdk-17-jdk openjdk-17-jre
 jar_url="https://www.dropbox.com/scl/fi/xyfr2ifpqi5znr9lcbkk0/CYOB.jar?rlkey=bx5bhf18v6uyyxgh7y39n89qv&dl=1"
 
 # Загрузка JAR файла
-wget "$jar_url" -O CYOB.jar
+wget "$jar_url" -O $HOME/CYOB.jar
 
+# Создание .desktop файла
+echo "[Desktop Entry]" > "$desktop_file"
+echo "Type=Application" >> "$desktop_file"
+echo "Name=$program_name" >> "$desktop_file"
+echo "Exec=$program_exec" >> "$desktop_file"
+echo "Hidden=false" >> "$desktop_file"
+echo "NoDisplay=false" >> "$desktop_file"
+echo "X-GNOME-Autostart-enabled=true" >> "$desktop_file"
 
-# Запуск программы
-java -jar CYOB.jar
+echo "Создан $desktop_file для автозагрузки программы $program_name."
+
+# Запуск программы в новом screen
+screen -dmS $program_name bash -c "$program_exec"
